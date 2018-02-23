e65a0c6ead7a7a52f2c72a20f9dd03dd4da025b2
MaeganW <maeganwomble@gmail.com>
2018-02-23 09:22:46 -0600

    wip

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const http = require('http').Server(app);
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+const port = process.env.PORT || '4200';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+http.listen(port, () => console.log(`API running on localhost:${port}`));
+
+process.on('uncaughtException', function(err){
+  console.log(err);
+})
